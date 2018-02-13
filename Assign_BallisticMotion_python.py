f6d8bdb8132b8bd003bc56013e846d862d594533
Haplescent <johnmerritt95@gmail.com>
2018-02-13 12:22:46 -0500

    update with matplotlib

--- Assign_BallisticMotion_python.py
+++ Assign_BallisticMotion_python.py
@@ -3,8 +3,12 @@ Goal: Create a python program that iteratively calculates and visualizes the mot
 of an object in a constant gravitational field.
 """
 
+
 import math as ma
 from sys import argv
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 """
 Requirement: The iterative calculation may only deviate from the analytic trajectory by
@@ -14,24 +18,56 @@ sheet(s).
 
 script, g_const, v0_mag, v0_ang_degree, y0, ts = argv
 
-v0_ang = float(v0_ang_degree) / 180.00 * ma.pi
-v0x = float(v0_mag) * ma.cos(float(v0_ang))
-v0y = float(v0_mag) * ma.sin(float(v0_ang))
+#floating all numbers
+g_const = float(g_const)
+v0_mag = float(v0_mag)
+v0_ang_degree = float(v0_ang_degree)
+y0 = float(y0)
+ts = float(ts)
+
+#time indepedent calculations
+v0_ang = (v0_ang_degree) / 180.00 * ma.pi
+v0x = (v0_mag) * ma.cos((v0_ang))
+v0y = (v0_mag) * ma.sin((v0_ang))
 ax = 0.0
-ay = - (float(g_const))
+ay = - ((g_const))
 
 flytime = (2.0 * v0y) / (-ay)
 dis = flytime * v0x
 th = v0y / - ay
-max_y = ((v0y) ** 2.0 / (2.0 * -ay) + float(y0))
+max_y = ((v0y) ** 2.0 / (2.0 * -ay) + (y0))
 
-print v0x
-print v0y
-print ax
-print ay
+print "Max height is"
+print max_y, "meters"
+print "Total distance traveled is"
+print dis, "meters"
+print "object is in the air for"
+print flytime, "secs"
 
-print flytime
-print max_y
-print dis
-print flytime
+analytic_y_plot_list = []
+analytic_x_plot_list = []
+def gen_analytic_y_plot_list(y0, v0y, ay, ts, v0x):
+	t = 0.0
+	while t <= flytime + ts:
+		analytic_y_plot_list.append(y0 + v0y * t + (1.0 / 2.0) * (ay) * t ** 2)
+		analytic_x_plot_list.append(v0x * t)
+		t = t + ts
+	return analytic_x_plot_list, analytic_y_plot_list
+
+gen_analytic_y_plot_list(y0, v0y, ay, ts, v0x)
+
+y = np.asarray(analytic_y_plot_list)
+x = np.asarray(analytic_x_plot_list)
+
+
+plt.plot(x, y)
+plt.xlim(0.0, dis)
+plt.ylim(0.0, max_y)
+
+plt.xlabel(r"$x-position$")
+plt.ylabel(r"$y-position$")
+plt.show()
+
+
+
 
